8eed186e68ecbd8ad80e9c4debcb8a44b0254056
soup <LuisATorres0001@gmail.com>
2023-10-20 20:15:43 -0700

    woot woot

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ function rngRGB() {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
-    return `rgb(${r}, ${g}, ${b})`
+    return `rgb(${r},${g},${b})`
 }
 
 const letters = document.querySelectorAll('.letter');
 const intervalid = setInterval(function () {
     for (let letter of letters) {
-        letter.computedStyleMap.color = randomRGB();
+        letter.style.color = rngRGB();
     }
 }, 500);
